fd907a5af6668b9a6997b9c32093973e53247660
jankey <18311271399@163.com>
2017-12-10 22:17:37 +0800

    add app

--- controllers/app.go
+++ controllers/app.go
@@ -1,1 +1,37 @@
 package controllers
+
+type ChannelController struct {
+	BaseController
+}
+
+type ApplicationController struct {
+	BaseController
+}
+
+type PkgController struct {
+	BaseController
+}
+
+type VersionController struct {
+	BaseController
+}
+
+type CodeController struct {
+	BaseController
+}
+
+type EnvController struct {
+	BaseController
+}
+
+type BuildController struct {
+	BaseController
+}
+
+type TypeController struct {
+	BaseController
+}
+
+type AppController struct {
+	BaseController
+}
--- models/app.go
+++ models/app.go
@@ -1,1 +1,112 @@
 package models
+
+type Channel struct {
+	Id         int
+	Name       string
+	FriendId   string
+	Descript   string
+	CreateId   int
+	UpdateId   int
+	CreateTime int64
+	UpdateTime int64
+	Views      int
+}
+
+type Application struct {
+	Id         int
+	Name       string
+	Descript   string
+	CreateId   int
+	UpdateId   int
+	CreateTime int64
+	UpdateTime int64
+	Views      int
+}
+
+type Pkh struct {
+	Id         int
+	Name       string
+	Descript   string
+	CreateId   int
+	UpdateId   int
+	CreateTime int64
+	UpdateTime int64
+	Views      int
+}
+
+type Version struct {
+	Id         int
+	Name       string
+	Descript   string
+	CreateId   int
+	UpdateId   int
+	CreateTime int64
+	UpdateTime int64
+	Views      int
+}
+
+type Code struct {
+	Id         int
+	Code       int
+	Descript   string
+	CreateId   int
+	UpdateId   int
+	CreateTime int64
+	UpdateTime int64
+	Views      int
+}
+
+type Env struct {
+	Id         int
+	Name       string
+	Descript   string
+	CreateId   int
+	UpdateId   int
+	CreateTime int64
+	UpdateTime int64
+	Views      int
+}
+
+type Build struct {
+	Id         int
+	Name       string
+	Descript   string
+	CreateId   int
+	UpdateId   int
+	CreateTime int64
+	UpdateTime int64
+	Views      int
+}
+
+type Type struct {
+	Id         int
+	Name       string
+	Descript   string
+	CreateId   int
+	UpdateId   int
+	CreateTime int64
+	UpdateTime int64
+	Views      int
+}
+
+type App struct {
+	Id          int
+	Type        string
+	Application string
+	Pkg         string
+	Version     string
+	Code        int
+	Env         string
+	Build       string
+	Channel     string
+	FriendId    string
+	Descript    string
+	Times       int
+	Url         string
+	Downs       int
+	CreateId    int
+	UpdateId    int
+	CreateTime  int64
+	UpdateTime  int64
+	Views       int
+}
